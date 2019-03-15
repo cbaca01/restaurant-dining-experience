7c742d2d6e31cb21aeacb0099f2a436caa002fe1
Chris E. Baca <cbaca01@gmail.com>
2019-03-15 12:08:41 -0400

    Created api file
    Created main api file where our application which will act as a controller to output the restaurants we want to see that have a grade of 80

---
+++ app.py
@@ -0,0 +1,59 @@
+from flask import Flask, jsonify
+from includes.database import *
+app = Flask(__name__)
+
+session = Session(engine)
+
+@app.route("/getdata")
+def getData():
+	data = {}
+
+	query = """
+		SELECT r.camis_id,
+			   r.dba,
+			   b.name AS boro,
+			   r.building,
+			   r.street,
+			   r.zip_code,
+			   r.phone,
+			   to_char(i.inspect_date, 'mm/dd/yyyy'),
+			   c.description AS cuisine_description,
+			   a.description AS action,
+			   v.code AS violation_code,
+			   v.description AS violation_description,
+			   cf.description AS critical_flag,
+			   i.score,
+			   g.label AS grade,
+			   to_char(i.grade_date, 'mm/dd/yyyy'),
+			   to_char(i.record_date, 'mm/dd/yyyy'),
+			   it.description AS inspection_type
+		FROM inspections i
+		INNER JOIN restaurants r ON i.restaurant_id = r.camis_id
+		INNER JOIN boros b ON r.boro_id = b.id
+		INNER JOIN cuisines c ON r.cuisine_id = c.id
+		INNER JOIN actions a ON i.action_id = a.id
+		INNER JOIN violation_codes v ON i.violation_id = v.id
+		INNER JOIN critical_flags cf ON i.critical_flag_id = cf.id
+		INNER JOIN grades g ON i.grade_id = g.id
+		INNER JOIN inspection_types it ON i.inspect_type_id = it.id
+		WHERE g.score >= :grade_score
+		LIMIT 10
+	"""
+	qParams = {'grade_score': 80}
+
+	results = session.execute(query, qParams)
+
+	data = []
+	for i, row in enumerate(results):
+		data.append(dict(zip(row.keys(), row)))
+
+
+	return jsonify(data)
+ 
+if __name__ == "__main__":
+	app.run()
+
+
+
+
+
