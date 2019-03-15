02c5109848ebecce4e13f88e094b79b241daf2c5
Chris E. Baca <cbaca01@gmail.com>
2019-03-15 12:55:58 -0400

    Added database connection files
    Made the following changes:
    - Added database.py: this will handle connecting to our databsae
    - Modified config.env so that no database credentials are present in the file.  This was done for security.
    NOTE:  You will need to put this in .gitignore and added your database crendentials.

---
+++ includes/database.py
@@ -0,0 +1,42 @@
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+
+from sqlalchemy import create_engine, select, MetaData, Table, null
+from sqlalchemy.orm import Session, sessionmaker
+from sqlalchemy.ext.automap import automap_base
+
+# Loading config.env file to get our data
+dotenv_path = join(dirname(__file__), 'config.env')
+load_dotenv(dotenv_path)
+
+# retrieving our database connection parameters
+DBHOST  	 = os.getenv('DBHOST')
+DBNAME 		 = os.getenv('DBNAME')
+DBUSER  	 = os.getenv('DBUSER')
+DBPASSWORD   = os.getenv('DBPASSWORD')
+
+Base = automap_base()
+
+engine = create_engine('postgresql://' + DBUSER + ':' + DBPASSWORD + '@' + DBHOST + '/' + DBNAME)
+connection = engine.connect()
+
+metadata = MetaData(bind=None)
+
+# Reflect tables
+Base.prepare(engine, reflect=True)
+
+# Tables we use as List Tables
+boros = Base.classes.boros
+cuisines = Base.classes.cuisines
+actions = Base.classes.actions
+violation_codes = Base.classes.violation_codes
+inspection_types = Base.classes.inspection_types
+critical_flags = Base.classes.critical_flags
+grades = Base.classes.grades
+
+# Junction Tables
+restaurants = Base.classes.restaurants
+
+# Main table
+inspections = Base.classes.inspections
